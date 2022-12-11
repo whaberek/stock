cef3bc57ccbc681207c78a8af92c988b2d629540
Wojciech Haberek <wojciech@kramphub.com>
2022-12-11 22:35:49 +0100

    fix: HashRouter must be used

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,32 @@ import React, { memo } from 'react';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import { Navbar } from 'components/Navbar';
-import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Container } from 'App.styles';
 import { Dashboard } from 'scenes/dashboard';
 import { Companies } from 'scenes/companies';
 
 export const App = memo(() => {
   return (
-    <BrowserRouter>
-      <HashRouter basename="/">
-        <Navbar/>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) => theme.palette.grey[100],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar/>
-          <Container>
-            <Routes>
-              <Route path="/" element={<Dashboard/>}/>
-              <Route path="/companies" element={<Companies/>}/>
-            </Routes>
-          </Container>
-        </Box>
-      </HashRouter>
-    </BrowserRouter>
+    <HashRouter basename="/">
+      <Navbar/>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) => theme.palette.grey[100],
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto',
+        }}
+      >
+        <Toolbar/>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path="/companies" element={<Companies/>}/>
+          </Routes>
+        </Container>
+      </Box>
+    </HashRouter>
   );
 });
